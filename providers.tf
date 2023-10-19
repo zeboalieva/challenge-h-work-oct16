4013a5830b0ab38aa4e010d249ecf62b9edd9beb
Zebo Alieva <zeboalieva@air-zebo.mynetworksettings.com>
2023-10-18 20:31:31 -0400

    hard challenge lab that i'l  tried to understand

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud {
+    organization = "tf-class-september-20"
+
+    workspaces {
+      name = "lab-oct-16"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
